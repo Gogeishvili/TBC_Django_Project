9e2574350b250f8d064b67233c38401c0f491528
Gogeishvili <gio.gogeishvili@gmail.com>
2024-10-13 18:49:04 +0400

    Merge

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django_project import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
@@ -24,3 +27,7 @@ urlpatterns = [
     path("order/", include("order.urls")),
     path("", admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_project/store/urls.py
+++ django_project/store/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from django_project import settings
 from store import views
-from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -10,5 +8,4 @@ urlpatterns = [
     path("category/info/", views.categories, name="categories"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
