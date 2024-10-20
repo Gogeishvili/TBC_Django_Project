beaef72813caa1e5d89c4dfacb36229b01f3e3e3
Gogeishvili <gio.gogeishvili@gmail.com>
2024-10-20 22:27:14 +0400

    created new user app and user model, also vreated new model User card in Iored app

--- django_project/order/models.py
+++ django_project/order/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class UserCard(models.Model):
+    user = models.ForeignKey(User, verbose_name="user_card", on_delete=models.CASCADE)
--- django_project/user/views.py
+++ django_project/user/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 def user_main_page(request):
     return render(
         request, "userMainPage.html", {}
