e129d0ad6e1a448866b70f9ae475bbb41003974c
Gogeishvili <gio.gogeishvili@gmail.com>
2024-10-20 21:25:14 +0400

    created model admins

--- django_project/store/admin.py
+++ django_project/store/admin.py
@@ -1,6 +1,40 @@
 from django.contrib import admin
-from store.models import Category,Product
+from store.models import Category, Product
 
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "get_products")
+    list_per_page = 2
+    search_fields = ('name',)
+
+    @admin.display(description="Products")
+    def get_products(self, obj):
+        products = obj.products.all()
+        return ", ".join([product.name for product in products])
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "price",
+        "quantity",
+        "get_categories",
+        "is_active",
+        "get_total_price",
+    )
+    list_filter = ("category", "is_active")
+    search_fields = ("name", "category__name")
+    list_select_related = ()
+    list_editable = ("price", "quantity", "is_active")
+    list_per_page = 2
+    autocomplete_fields = ("category",)
+
+    @admin.display(description="Categories")
+    def get_categories(self, obj):
+        return ", ".join([category.name for category in obj.category.all()])
+
+    @admin.display(description="ჯამური ფასი")
+    def get_total_price(self, obj):
+        return obj.price * obj.quantity
