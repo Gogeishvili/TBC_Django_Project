92a2733df8350f69f7b7b0ecfb4f38a09dbb4b9a
Gogeishvili <gio.gogeishvili@gmail.com>
2024-10-22 23:30:19 +0400

    changed use manager class

--- django_project/user/models.py
+++ django_project/user/models.py
@@ -1,20 +1,7 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from user.managers import CustomUserManager
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, name, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, name=name, **extra_fields)
-        user.set_password(password)  
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, name, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)  
-        return self.create_user(email, name, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):  
     email = models.EmailField(unique=True)
