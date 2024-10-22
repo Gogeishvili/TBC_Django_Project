7198a41109afb966678b8e94532975507db77db8
Gogeishvili <gio.gogeishvili@gmail.com>
2024-10-22 23:20:19 +0400

    changed one 2 one connection user and user card

--- django_project/order/models.py
+++ django_project/order/models.py
@@ -4,7 +4,7 @@ from user.models import User
 
 class UserCard(models.Model):
     name = models.CharField(max_length=30, default="user card")
-    user = models.ForeignKey(User, verbose_name="user_card", on_delete=models.CASCADE)
+    user = models.OneToOneField(User, verbose_name="user_card", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user.name}'s {self.name}"
