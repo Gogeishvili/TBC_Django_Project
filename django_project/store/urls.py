2e6652f5370599b1185e59ee06a29a2a6a4e9317
Gogeishvili <gio.gogeishvili@gmail.com>
2024-10-24 03:09:16 +0400

    urls works correct

--- django_project/store/urls.py
+++ django_project/store/urls.py
@@ -4,16 +4,11 @@ from store import views
 app_name='store'
 
 urlpatterns = [
-    path('testHTML/',views.test_HTML,name='test_HTML'),
-    path('product/JSON/',views.product_JSON,name='product_JSON'),
-    path('category/JSON/',views.category_JSON,name='category_JSON'),
-    path("category/product/", views.product_of_category, name="products_of_category"),
-
-    #new urls
     path("", views.index, name="index"),
     path("category/", views.category, name="category"),
     path("product/", views.product, name="product"),
-    path("contact/",views.contact,name="contact")
+    path("contact/",views.contact,name="contact"),
+    path('test/',views.test_2,name='test_2'),
 ]
 
 
--- django_project/store/views.py
+++ django_project/store/views.py
@@ -4,68 +4,14 @@ from store.models import Product, Category
 from django.db.models import *
 
 
-def test_HTML(request):
-    products = Product.objects.prefetch_related("category").all()
-    categories = Category.objects.prefetch_related("products").all().annotate(products_count=Count('products'))
-    return render(
-        request, "test.html", {
-            "products": products,
-            "categories": categories}
-    )
 
 
-def product_JSON(request):
-    products = Product.objects.get_products_JSON()
-    return JsonResponse(products, safe=False)
-
-
-def category_JSON(request):
-    categories = Category.objects.get_category_JSON()
-    return JsonResponse(categories, safe=False)
-
-
-def product(request):
-    products = Product.objects.get_all_active_products()
-    return render(request, "product.html", {"products": products})
-
-
-def product_of_category(request):
-    category_summary = Category.objects.get_category_summary()
-    most_expensive = Category.objects.get_most_expensive_product()
-    cheapest = Category.objects.get_cheapest_product()
-    average_price = Category.objects.get_average_product_price()
-
-    return render(
-        request,
-        "productsOfCategory.html",
-        {
-            "category_summary": category_summary,
-            "most_expensive": most_expensive,
-            "cheapest": cheapest,
-            "average_price": average_price,
-        },
-    )
-def product_of_category(request):
-    category_summary = Category.objects.get_category_summary()
-    most_expensive = Category.objects.get_most_expensive_product()
-    cheapest = Category.objects.get_cheapest_product()
-    average_price = Category.objects.get_average_product_price()
-
-    return render(
-        request,
-        "productsOfCategory.html",
-        {
-            "category_summary": category_summary,
-            "most_expensive": most_expensive,
-            "cheapest": cheapest,
-            "average_price": average_price,
-        },
-    )
-
-#new views
+def test_2(request):
+    return render(request, "test2.html", {})
 
 def index(request):
-    return render(request,'index.html',{})
+    return render(request, "index.html", {})
+
 
 def category(request):
     categories_without_parent = Category.objects.filter(parent__isnull=True)
@@ -75,9 +21,11 @@ def category(request):
         {"categories_without_parent": categories_without_parent},
     )
 
+
 def product(request):
     products = Product.objects.get_all_active_products()
     return render(request, "shop-detail.html", {"products": products})
 
+
 def contact(request):
-    return render(request,'contact.html',{})
+    return render(request, "contact.html", {})
