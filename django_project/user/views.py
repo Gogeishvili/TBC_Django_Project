b0cf097338d7cd4e942fbf79a742279084095ec0
Gogeishvili <gio.gogeishvili@gmail.com>
2024-10-23 23:32:53 +0400

    start create base html

--- django_project/user/views.py
+++ django_project/user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def user_main_page(request,user_id):
     return render(
-        request, "user_main_page.html", {
+        request, "index.html", {
             'user_id':user_id
         }
     )
