270e4023cb2443b399353f60bea1ab4c16d2591d
Gogeishvili <gio.gogeishvili@gmail.com>
2024-10-27 00:07:10 +0400

    changed order admin

--- django_project/order/admin.py
+++ django_project/order/admin.py
@@ -8,11 +8,11 @@ class CartItemInline(admin.TabularInline):
     fields = ("product", "quantity", "get_product_price", "get_total_price")
     readonly_fields = ("get_product_price", "get_total_price")
 
+    @admin.display(description="Product Price")
     def get_product_price(self, obj):
         return obj.product.price if obj.product else "N/A"
 
-    get_product_price.short_description = "Product Price"
-
+    @admin.display(description="Total Price")
     def get_total_price(self, obj):
         return obj.product.price * obj.quantity if obj.product else "N/A"
 
@@ -24,12 +24,11 @@ class UserCartAdmin(admin.ModelAdmin):
     inlines = [CartItemInline]
     list_display = ("user", "get_total_cart_price", "product_count")
 
+    @admin.display(description="Total Cart Price")
     def get_total_cart_price(self, obj):
         return sum(item.product.price * item.quantity for item in obj.items.all())
 
-    get_total_cart_price.short_description = "Total Cart Price"
-
+    @admin.display(description="Total Products")
     def product_count(self, obj):
         return sum(item.quantity for item in obj.items.all())
 
-    product_count.short_description = "Total Products"
