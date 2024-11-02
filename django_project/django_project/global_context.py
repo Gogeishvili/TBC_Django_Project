1732bd28883a82f795903ca463976f5ac88b4d66
Gogeishvili <gio.gogeishvili@gmail.com>
2024-11-02 23:33:33 +0400

    creatde globa context cart items

--- django_project/django_project/global_context.py
+++ django_project/django_project/global_context.py
@@ -1,6 +1,13 @@
 
-def test_globa_context(request):
-    return{
-        'test_global_context':23
-    }
+from order.models import UserCart
 
+
+def cart_items(request):
+    total_items = 0
+    if request.user.is_authenticated:
+        cart = UserCart.objects.filter(user=request.user).first()
+        if cart:
+            total_items = sum(item.quantity for item in cart.items.all())
+    return {
+        'total_items': total_items,
+    }
